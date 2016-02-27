532478e444f305c192426589deb821c53576fbd7
JUAN <juanramirez25@hotmail.com>
2016-02-27 11:34:15 -0500

    Prueba1

---
+++ PruebaHolaMundoJJRR/src/main/java/beans/BeanSpringWeb.java
@@ -0,0 +1,15 @@
+package beans;
+
+public class BeanSpringWeb {
+	
+	private String mensaje;
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+}
